3e6daa40a5ea7708169270c67bfebff6905bf57b
Jon Packer <contact@jonpacker.com>
2015-04-24 12:37:14 +0200

    disable authentication

--- launch.sh
+++ launch.sh
@@ -3,6 +3,7 @@
 NEO4J_HOME=/var/lib/neo4j
 
 sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+sed -i "s|dbms.security.auth_enabled=true|dbms.security.auth_enabled=false|g" $NEO4J_HOME/conf/neo4j-server.properties
 
 # doing this conditionally in case there is already a limit higher than what
 # we're setting here. neo4j recommends at least 40000.
